8abfa13f8afba85e2a417b87818423f4f8145de8
AlexanderTheGr8tt <112800176+AlexanderTheGr8tt@users.noreply.github.com>
2023-03-04 23:20:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(
-  "mongodb://localhost:27017/todolistDB"
+  "mongodb+srv://aleksandermalecki1:qJUP2FRYC6su8d8@cluster0.ydoyvvv.mongodb.net/todolistDB"
 );
+
 console.log("Connected");
 
 // mongoose (Schema)
@@ -157,6 +158,10 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
   console.log("Server started on port 3000.");
 });
