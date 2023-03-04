d3db7ad480e546c668b47575db72b5c0b373967a
AlexanderTheGr8tt <112800176+AlexanderTheGr8tt@users.noreply.github.com>
2023-03-04 23:06:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect(
-  "mongodb://localhost:27017"
+  "mongodb://localhost:27017/todolistDB"
 );
 console.log("Connected");
 
